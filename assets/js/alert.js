c77325ded226050e777c64500ead0b970a436aa7
wtfTim <67240679+wtfTim@users.noreply.github.com>
2024-09-28 14:08:35 +0300

    Add files via upload

---
+++ assets/js/alert.js
@@ -0,0 +1,6 @@
+// 'use strict';
+// let age = prompt('Введите возраст', 'Неизветсный возсраст');
+// let isTrue = confirm(`Вами действительно ${age} лет`);
+// alert(isTrue)
+
+alert("Я JavaScript");
